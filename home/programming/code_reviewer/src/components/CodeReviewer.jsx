9e32b5cb083dce8724aa71c431acbd2edb3b0ddd
loverboy <loverboykosu@gmail.com>
2025-03-24 22:56:52 +0900

    feat: click event

--- home/programming/code_reviewer/src/components/CodeReviewer.jsx
+++ home/programming/code_reviewer/src/components/CodeReviewer.jsx
@@ -4,6 +4,8 @@ const CodeReviewer = () => {
   const [originWord, setOriginWord] = useState("");
   const getText = (e) => {
     setOriginWord(e.target.value);
+  };
+  const getClick = (e) => {
     setHasOriginWord(true);
   };
   return (
@@ -15,16 +17,25 @@ const CodeReviewer = () => {
       <div className="flex flex-row">
         {/* レビュー対象 */}
         <div className="relative origin basis-1/2 text-center m-3">
-          <h2 className="border-2 border-solid m-3 rounded">Original</h2>
+          <h2 className="border-2 border-solid w-7/8 max-x-md m-3 rounded">
+            Original
+          </h2>
           <textarea
-            className="text-white placeholder-white border-1 border-solid resize w-md min-h-80 bg-gray-900"
+            className="text-white placeholder-white w-full border-1 border-solid resize min-h-80 bg-gray-900"
             name=""
+            text-white
+            placeholder-white
+            w-full
+            border-
             id=""
             placeholder="レビューするスクリプトを入力してください"
             onChange={getText}
           ></textarea>
           <div className="absolute right-0">
-            <button className="rounded bg-gray-800 text-white p-2">
+            <button
+              className="rounded bg-gray-800 text-white p-2"
+              onClick={getClick}
+            >
               Check
             </button>
           </div>
@@ -32,11 +43,11 @@ const CodeReviewer = () => {
 
         {/* 翻訳後 */}
         <div className="origin basis-1/2 m-3">
-          <h2 className="border-2 border-solid m-3 text-center rounded">
+          <h2 className="border-2 border-solid m-3 w-7/8 text-center rounded">
             Result
           </h2>
-          <div className="border-1 border-solid w-md min-h-80 ">
-            {hasOriginWord && originWord}
+          <div className="border-1 border-solid w-full min-h-80 ">
+            {hasOriginWord ? originWord : null}
           </div>
         </div>
       </div>
